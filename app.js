bf3c890b3860a2dc46c17b4a554211965dec0f57
samual moahmed <sam-wal@windowslive.com>
2022-06-27 14:11:00 +0200

    add results div, add updates to results div in js
    added a div in index.html that displays results using javascript

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 //make the computer choose a random out of rock paper scissors
 const computerChoices = ['rock', 'paper', 'scissors'];
+// get buttons
+const buttons = document.querySelectorAll('button');
+// results div 
+const results = document.querySelector('#results');
+// tracking rounds of player and computer
+let playerWins = 0;
+let computerWins = 0;
 
+// make random move by computer
 function ComputerPlay()
 {
     let choice = Math.floor(Math.random() * computerChoices.length);
     return computerChoices[choice];
 }
+
+
 // take the player movement from the button clicked and register it
 function playerMove(buttonClick){
-    
+    console.log(`Player chose ${buttonClick.target.id}`);
+    Round(buttonClick.target.id);
 }
-// get buttons
-const buttons = document.querySelectorAll('button');
+
 
 // register onclick event for each button
 buttons.forEach((button) => button.addEventListener('click', playerMove));
 
 
 //make a round of rock paper scissors
-function Round(playerInput, computerInput)
+function Round(playerInput)
 {
+    computerInput = ComputerPlay();
+    console.log(`Computer chose ${computerInput}`);
     // check for draw
     if (playerInput == computerInput)
     {
+        UpdateScores();
         return 'draw';
     }
     // check if player losses this round
@@ -37,12 +50,20 @@ function Round(playerInput, computerInput)
 
     if (playerWon)
     {
-        return 'you won'; // player wins
+        console.log(`Player won this round total rounds won: ${++playerWins}`);
     }
     else
     {
-        return 'you lost' // player lost
+        console.log(`Computer won this round total rounds won: ${++computerWins}`);
     }
+
+    UpdateScores();
+}
+
+// update results div
+function UpdateScores()
+{
+    results.innerHTML = `<h2>Player Score: ${playerWins}</h2><h2>Computer Score: ${computerWins}</h2>`
 }
 //a game consists of 5 rounds in each round
 function game(){
@@ -83,4 +104,3 @@ function game(){
 }
 
 // start the game
-game();
